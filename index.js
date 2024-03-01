6f3405fcdbc05af40a41b0a2c56974c1bfe470e1
Ankit chandore <ankitchandore997@gmail.com>
2024-03-01 15:17:28 +0530

    project setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const cors = require("cors")
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.json())
+app.use(cors())
+app.use(bodyparser.urlencoded({ extended : true })); 
+
+
+app.listen(3000)
